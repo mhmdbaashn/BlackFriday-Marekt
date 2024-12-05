4cc560bab63a6b981b128cc711faf85590736971
Mohammed Baashn <ir.mnmn.maob@gmail.com>
2024-12-05 19:15:34 +0000

    Added log in as user

--- market/urls.py
+++ market/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home',),
     path('product_list/', views.product_list, name='product_list'),
     path('<int:pk>/', views.product_detail, name='product_detail'),
     path('new/', views.product_create, name='product_create'),
